bf3093e2a9b661886d325000e626f217ac93b4e5
tosinsalau <tosin.salau01@gmail.com>
2025-03-26 12:32:34 -0400

    updates

--- tests/testthat/test-srm_plots.R
+++ tests/testthat/test-srm_plots.R
@@ -1,7 +1,15 @@
-
-test_that("srm_plots works", {
+test_that("srm_plot works", {
+  # Create a sample matrix
+  test_matrix <- matrix(c(0, 1, 2, 
+                          2, 0, 1, 
+                          1, 2, 0), nrow=3, ncol=3)
+  rownames(test_matrix) <- c("A", "B", "C")
+  colnames(test_matrix) <- c("A", "B", "C")
   
-  #test code here
+  # Calculate actor effects
+  actor_effects <- srm_effects(test_matrix, type="actor")
   
-  expect_true(TRUE)
+  # Test actor plot only
+  actor_plot <- srm_plot(actor_effects, type="actor", n=3)
+  expect_true(inherits(actor_plot, "ggplot"))
 })
